1ec5bbe04638c9cb661fe344ceff1392ffb5216e
YaKumagai <yasuo.kumagai@kameyanet.co.jp>
2021-02-25 18:02:46 +0900

    2021年2月25日18:00 commit

--- index.php
+++ index.php
@@ -1,21 +1,3 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ペットショップ</title>
-</head>
-<body>
-    <h1>本日のご紹介ペット</h1>
-    <form method="get">
-        <label>キーワード：</label>
-        <input name="<?= h("keyword") ?>" placeholder="キーワードを入力下さい" size="24" type="text">
-        <input type="submit" value="検索">
-        <?php echo '「' . h($_GET["keyword"]) . '」で、曖昧検索しました'; ?>
-    </form>
-</body>
-</html>
-
 <?php
 // 特殊文字をHTMLエンティティに変換
 function h($str){
@@ -35,12 +17,12 @@ try {
     exit;
 }
 
-$keyword = h($_GET["keyword"]);
+// 曖昧検索とSQL文の組み立て
+$keyword = $_GET["keyword"];
 if (empty($keyword)) {
     $sql = 'SELECT id, description, type, classifcation, birthplace, birthday ' . 
         'FROM animals ORDER BY id ASC';
 } else {
-    // SQL文の組み立て
     $keyword = '%' . $keyword . '%';
     $sql = 'SELECT id, description, type, classifcation, birthplace, birthday ' .
         'FROM animals WHERE description LIKE :keyword ORDER BY id ASC';
@@ -56,22 +38,34 @@ $stmt->execute();
 
 // 結果の受け取り
 $animals = $stmt->fetchAll(PDO::FETCH_ASSOC);
+?>
 
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ペットショップ</title>
+</head>
+<body>
+    <h1>本日のご紹介ペット</h1>
+    <form method="get">
+        <label>キーワード：</label>
+        <input name="keyword" placeholder="キーワードを入力下さい" size="24" type="text">
+        <input type="submit" value="検索">
+        <?php echo '「' . h($_GET["keyword"]) . '」で、曖昧検索しました'; ?>
+    </form>
+</body>
+</html>
+
+<?php
+// レコードの表示
 foreach ($animals as $animal) {
-    echo '<br>' . $animal['type'] . 'の' .
+    echo $animal['type'] . 'の' .
         $animal['classifcation'] . 'ちゃん' . '<br>' .
         $animal['description'] . '<br>' .
         $animal['birthday'] . '生まれ' . '<br>' .
         '出身地' . $animal['birthplace'] . '<hr>';
 }
 
-/*
-<script>alert('警告！！！！');</script>
-
-【XSS対策補足】
-htmlspecialchars関数 を利用したことで、<script>alert('警告！！！');</script> は
-コードとして認識されず、文字列として吐き出されたということになります。
-セキュリティはとても重要なことなので必ず理解出来るようにしておきましょう。
-
-*/
 // http://localhost/php_exercise/pet_shop/index.php
